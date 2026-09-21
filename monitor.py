529a907f8154493696301295744c96a41b9902d9
1095is <82995216+1095is@users.noreply.github.com>
2026-09-21 20:53:17 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,1 +1,20 @@
-print("Avito Monitor запущен")
+import os
+import urllib.parse
+import urllib.request
+
+token = os.environ["TELEGRAM_BOT_TOKEN"]
+chat_id = os.environ["TELEGRAM_CHAT_ID"]
+
+message = "🎉 Работает! Твой Avito-бот успешно подключён к Telegram."
+
+url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+data = urllib.parse.urlencode({
+    "chat_id": chat_id,
+    "text": message
+}).encode()
+
+request = urllib.request.Request(url, data=data)
+
+with urllib.request.urlopen(request) as response:
+    print(response.read().decode())
